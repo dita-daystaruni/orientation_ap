a55d1e1f48b781c7755fac9b7c64b20900f66486
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-08-27 14:04:41 +0300

    feat: connected faqs to the backend

--- lib/pages/faq_page.dart
+++ lib/pages/faq_page.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:lottie/lottie.dart';
 import 'package:orientation_app/constants/custom_colors.dart';
 import 'package:orientation_app/constants/custom_icons/custom_icons.dart';
+import 'package:orientation_app/controllers/faqs_controller.dart';
 import 'package:orientation_app/widgets/documents_tile.dart';
 import 'package:orientation_app/widgets/faqtile.dart';
 import 'package:orientation_app/widgets/text_icon.dart';
 
-class FaqPage extends StatelessWidget {
+class FaqPage extends StatefulWidget {
   const FaqPage({
     super.key,
     this.isParent = false,
@@ -19,63 +22,112 @@ class FaqPage extends StatelessWidget {
   ];
 
   @override
+  State<FaqPage> createState() => _FaqPageState();
+}
+
+class _FaqPageState extends State<FaqPage> with SingleTickerProviderStateMixin {
+  late FaqController faqController;
+  late TabController tabController;
+
+  @override
+  void initState() {
+    // faq controller
+    faqController = Get.find<FaqController>();
+    // tab controller
+    tabController = TabController(length: 2, vsync: this);
+    tabController.addListener(
+      () {
+        // TODO invoke silent updates for FAQS and Documnets, a way to restrict to many changes
+        debugPrint("Changing Page");
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // FAQ Controller
+
     // putting routines and sessions controllers
-    return DefaultTabController(
-      length: myTabs.length,
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: CustomColors.backgroundColor,
+      appBar: AppBar(
         backgroundColor: CustomColors.backgroundColor,
-        appBar: AppBar(
-          backgroundColor: CustomColors.backgroundColor,
-          title: const Center(
-            child: Text(
-              "Routines & Sessions",
-              style: TextStyle(
-                color: CustomColors.textColor,
-                fontSize: 20,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-          ),
-          bottom: const TabBar(
-            padding: EdgeInsets.all(8.0),
-            indicatorColor: CustomColors.buttonColor,
-            dividerColor: CustomColors.indicatorColor,
-            labelColor: CustomColors.buttonColor,
-            labelStyle: TextStyle(
+        title: const Center(
+          child: Text(
+            "Routines & Sessions",
+            style: TextStyle(
               color: CustomColors.textColor,
+              fontSize: 20,
               fontWeight: FontWeight.w500,
-              fontSize: 16,
             ),
-            overlayColor: WidgetStatePropertyAll(
-              CustomColors.hoverColor,
-            ),
-            tabs: myTabs,
           ),
-          automaticallyImplyLeading: false,
         ),
-        body: TabBarView(children: [
+        bottom: TabBar(
+          controller: tabController,
+          padding: const EdgeInsets.all(8.0),
+          indicatorColor: CustomColors.buttonColor,
+          dividerColor: CustomColors.indicatorColor,
+          labelColor: CustomColors.buttonColor,
+          labelStyle: const TextStyle(
+            color: CustomColors.textColor,
+            fontWeight: FontWeight.w500,
+            fontSize: 16,
+          ),
+          overlayColor: const WidgetStatePropertyAll(
+            CustomColors.hoverColor,
+          ),
+          tabs: FaqPage.myTabs,
+        ),
+        automaticallyImplyLeading: false,
+      ),
+      body: TabBarView(
+        controller: tabController,
+        children: [
           // faq tab view
           Column(
             children: [
-              // list of faqs
-              Expanded(
-                  child: ListView.builder(
-                itemBuilder: (context, index) {
-                  return const Padding(
-                    padding: EdgeInsets.all(8.0),
-                    child: FaqTile(),
-                  );
-                },
-                itemCount: 10,
-              ))
+              Obx(
+                () => faqController.faqs.isNotEmpty
+                    ?
+                    // list of faqs
+                    Expanded(
+                        child: ListView.builder(
+                          itemBuilder: (context, index) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: FaqTile(
+                                question: faqController.faqs[index].question,
+                                answer: faqController.faqs[index].answer,
+                              ),
+                            );
+                          },
+                          itemCount: faqController.faqs.length,
+                        ),
+                      )
+                    : Column(
+                        children: [
+                          Lottie.asset(
+                            "assets/lotties/error.json",
+                          ),
+                          const Text(
+                            "NO FAQS YET",
+                            style: TextStyle(
+                              color: CustomColors.secondaryTextColor,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+              ),
             ],
           ),
           // Documents Page
           Column(
             children: [
               // if user is a parent gives functionality to upload
-              isParent
+              widget.isParent
                   ? Padding(
                       padding: const EdgeInsets.symmetric(
                         horizontal: 50,
@@ -113,7 +165,7 @@ class FaqPage extends StatelessWidget {
               ))
             ],
           ),
-        ]),
+        ],
       ),
     );
   }
