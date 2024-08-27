882f9d07be6a5ecdd4ea9d739d7f33960e19d52b
FIRST_NAME LAST_NAME <rodney002mwanje@gmail.com>
2024-08-27 13:50:38 +0300

    mod: added more controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_navigation/get_navigation.dart';
 import 'package:orientation_app/constants/routes.dart';
+import 'package:orientation_app/controllers/activites_session_controller.dart';
 import 'package:orientation_app/controllers/contacts_controller.dart';
+import 'package:orientation_app/controllers/faqs_controller.dart';
 
 void main() {
-    // Putting controllers
+  // Putting controllers
   Get.put(UserContactController());
+  Get.put(FaqController());
+  Get.put(ActivitySessionController());
 
   runApp(
     GetMaterialApp(
